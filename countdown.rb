31291f6f9b20513eb9614f1d5024e7124ba36b2e
Ryan Ditzel <ryand370@gmail.com>
2019-04-29 00:23:58 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,6 +10,12 @@ def countdown(number_of_times)
 end
 
 def countdown_with_sleep
+  x = number_of_times
+  while x > 0 do 
+    puts "#{x} SECOND(S)!"
+    x -= 1 
+  end
+  "HAPPY NEW YEAR!"
   sleep(5)
 end
-
+end

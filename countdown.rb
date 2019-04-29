b16c1ec328424de5da6c58f473916be5b052516a
Ryan Ditzel <ryand370@gmail.com>
2019-04-29 00:14:21 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
-def countdown
+def countdown(number_of_times)
+  x = number_of_times
+  while x > 0 do 
+    puts "#{number} SECOND(S)!"
+    x -= 1 
+  end
+  puts "HAPPY NEW YEAR!"
 end
+
